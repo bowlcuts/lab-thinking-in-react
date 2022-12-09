4f5f0eba72f4f3d708dc35a1946a10fe6a34f60e
okay.enrique@gmail.com <okay.enrique@gmail.com>
2022-12-09 15:55:04 -0500

    lab done

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -6,11 +6,31 @@ import jsonData from '../data.json';
 const ProductsPage = () => {
     
     const [products, setProducts] = useState(jsonData);
-    
+    const [copyProducts, setCopyProducts] = useState([...products])
+    const searchProducts = (myInput) => {
+        const foundProduct = copyProducts.filter((product) => {
+            return product.name.toLowerCase().includes(myInput.toLowerCase())
+        })
+        setProducts(foundProduct)
+    }
+
+    const itemsStock = (checked) => {
+        if(checked){
+            const stockItem = copyProducts.filter((product) => {
+                return product.inStock === true;
+            })
+            setProducts(stockItem)
+        }else {
+            setProducts(copyProducts)
+        }
+    }
+
+
+
     return (
         <div>
             <h1>IronStore</h1>
-            <SearchBar />
+            <SearchBar searchProducts = {searchProducts} itemsStock={itemsStock}/>
             <ProductTable products= {products}/>
         </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,14 +4,30 @@ const SearchBar = (props) => {
 
     const [search, setSearch] = useState('');
 
-    const [isChecked]
+    const [isChecked, setIsChecked] = useState(false);
+
+    const searchChange = (event) => {
+        setSearch(event.target.value)
+        props.searchProducts(event.target.value)
+    }
 
     return (
+        <>
         <div>
             <label>Search</label>
-            <input></input>
-            <button>Search</button>
+            <input onChange={searchChange}
+            type="text"
+            value={search}
+            /> 
         </div>
+        <div>
+            <input type="checkbox" value={isChecked} onChange={(element) => {
+                setIsChecked(!isChecked)
+                props.itemsStock(!isChecked)
+            }} />
+            <p>In stock</p>
+        </div>
+        </>
     );
 };
 
